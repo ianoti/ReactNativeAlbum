f9fd1e53d35aad2cad9c0e9e7b9551be0353bec7
ianoti <ianodhiambootieno@gmail.com>
2017-08-18 12:44:16 +0300

    add axios for requests

--- src/components/album-list.js
+++ src/components/album-list.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album list</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = ({ albumList: [] });
+  }
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then((response) => {
+        this.setState({ albumList: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <View>
+        <Text>Album list</Text>
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
